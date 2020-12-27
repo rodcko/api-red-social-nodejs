1fe176bc72128c57cf80724f66b527bb160ca44c
unknown <rodcko@live.com>
2020-12-27 16:44:07 -0600

    Aislar el codigo de la base de datos

---
+++ store/dummy.js
@@ -0,0 +1,30 @@
+const db = {
+    'user': [
+        { id: 1, name: 'Rodcko'},
+    ],
+};
+
+function list(tabla) {
+    return db[tabla];
+}
+
+function get(tabla, id) {
+    let col = list(tabla);
+    return col.filter(item => item.id === id) [0] || null;
+}
+
+function upsert(tabla, data) {
+    db[collection].push(data);
+
+}
+
+function remove(tabla, id) {
+    return true;
+}
+
+module.exports = {
+    list,
+    get,
+    upsert,
+    remove,
+}
