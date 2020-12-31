72bc23a8440a4e65743544ac0c7d51ce29677a72
unknown <rodcko@live.com>
2020-12-30 22:40:09 -0600

    Base de datos real: MySQL

--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        user: process.env.MYSQL_USER || 'i2empQbkdq',
+        password: process.env.MYSQL_PASS || 'tWRQWO45f1',
+        database: process.env.MYSQL_DB || 'i2empQbkdq',
+    },
 }
