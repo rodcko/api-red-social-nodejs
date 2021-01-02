abc2b460c10be3d0742fd66dd86a6ab7b149f8b4
unknown <rodcko@live.com>
2021-01-01 19:40:59 -0600

    Conectando con nuestro microservicio de datos

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ module.exports = {
         database: process.env.MYSQL_DB || 'i2empQbkdq',
     },
     mysqlService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
         port: process.env.MYSQL_SRV_PORT || 3001,
     }
 }
